7bf56024b52135cd1224e82d8f0400ac68c109d1
Marco Braak <mbraak@ridethepony.nl>
2014-08-07 09:44:44 +0200

    Mention Python 3.4 in setup.py

--- setup.py
+++ setup.py
@@ -22,5 +22,6 @@ setup(
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
     ]
 )
