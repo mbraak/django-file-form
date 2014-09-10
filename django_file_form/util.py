d7297f0845d41ddc3eac07d07386e4b1c85c6d5a
Marco Braak <mbraak@ridethepony.nl>
2014-09-10 15:40:07 +0200

    Use get_queryset; also added fallback for Django < 1.6

--- django_file_form/util.py
+++ django_file_form/util.py
@@ -1,10 +1,23 @@
+import django
 from django.db import models
+
+
+def get_short_django_version():
+    """
+    Get first two numbers of Django version.
+    E.g. (1, 5)
+    """
+    return django.VERSION[0:2]
 
 
 class ModelManager(models.Manager):
     def try_get(self, **kwargs):
-        qs = self.get_query_set().filter(**kwargs)
+        qs = self.get_queryset().filter(**kwargs)
         if qs.exists():
             return qs.get()
         else:
             return None
+
+    if get_short_django_version() < (1, 6):
+        def get_queryset(self, *args, **kwargs):
+            return self.get_query_set(*args, **kwargs)
