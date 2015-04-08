77ba17b2cb0cdc3c274c3f8a7875719735c9db68
Marco Braak <mbraak@ridethepony.nl>
2015-04-08 14:10:20 +0200

    Use BaseCommand instead of NoArgsCommand

--- django_file_form/management/commands/delete_unused_files.py
+++ django_file_form/management/commands/delete_unused_files.py
@@ -1,12 +1,12 @@
 from __future__ import print_function
 
-from django.core.management.base import NoArgsCommand
+from django.core.management.base import BaseCommand
 
 from django_file_form.models import UploadedFile
 
 
-class Command(NoArgsCommand):
-    def handle_noargs(self, **options):
+class Command(BaseCommand):
+    def handle(self, *args, **options):
         deleted_files = UploadedFile.objects.delete_unused_files()
 
         verbosity = options.get('verbosity')
