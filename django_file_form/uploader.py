6991f500453b2852a78dc629539e3e76468bc350
Marco Braak <mbraak@ridethepony.nl>
2017-03-21 11:18:30 +0100

    Set FileFormUploadBackend.UPLOAD_DIR

--- django_file_form/uploader.py
+++ django_file_form/uploader.py
@@ -11,6 +11,8 @@ from .util import load_class
 
 
 class FileFormUploadBackend(LocalUploadBackend):
+    UPLOAD_DIR = conf.UPLOAD_DIR
+
     def upload_complete(self, request, filename, file_id, *args, **kwargs):
         result = super(FileFormUploadBackend, self).upload_complete(
             request, filename, file_id, *args, **kwargs)
@@ -22,7 +24,7 @@ class FileFormUploadBackend(LocalUploadBackend):
             original_filename = request.FILES['qqfile'].name
 
         values = dict(
-            uploaded_file=os.path.join(conf.UPLOAD_DIR, filename),
+            uploaded_file=os.path.join(self.UPLOAD_DIR, filename),
             file_id=file_id,
             form_id=request.POST['form_id'],
             original_filename=original_filename,
