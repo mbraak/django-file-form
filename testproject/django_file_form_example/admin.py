cbd1dfa805ad6ab96f96b95d211dd9d191188929
Marco Braak <mbraak@ridethepony.nl>
2017-03-24 15:00:44 +0100

    Add ExampleFile inline to Example2 model in admin

--- testproject/django_file_form_example/admin.py
+++ testproject/django_file_form_example/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 
-from .models import Example, Example2
+from .models import Example, Example2, ExampleFile
+
+
+class ExampleFileInline(admin.TabularInline):
+    model = ExampleFile
+
+
+class Example2Admin(admin.ModelAdmin):
+    inlines = [ExampleFileInline]
 
 
 admin.site.register(Example)
-admin.site.register(Example2)
+admin.site.register(Example2, Example2Admin)
