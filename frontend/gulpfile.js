86c6a62d7b662cca430e7a1b3830247e18561bd7
Marco Braak <marco@invoicefinance.nl>
2019-01-01 09:49:09 +0100

    Update gulp to 4.0

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -2,14 +2,24 @@ const gulp = require("gulp");
 const babel = require("gulp-babel");
 const uglify = require("gulp-uglify");
 
-
-gulp.task("default", () =>
+gulp.task("default", () => (
     gulp.src("file_form.js")
         .pipe(babel())
         .pipe(uglify())
         .pipe(gulp.dest("../django_file_form/static/file_form/"))
+  )
 );
 
-gulp.task("watch", ["default"], () => {
-    gulp.watch(["./*.js"], ["default"]);
-});
+gulp.task(
+  "watch",
+  gulp.series(
+    "default",
+    done => {
+      gulp.watch(
+        "./*.js",
+        gulp.series("default")
+      );
+      done();
+    }
+  )
+);
