ed14233a6a34a55f18673f6a23c57054de276963
Marco Braak <marco@invoicefinance.nl>
2019-04-05 18:10:49 +0200

    Remove Django 1.11 and Python 2.7 from setup

--- setup.py
+++ setup.py
@@ -25,13 +25,11 @@ setup(
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Framework :: Django",
-        "Framework :: Django :: 1.11",
         "Framework :: Django :: 2.0",
         "Framework :: Django :: 2.1",
         "Framework :: Django :: 2.2",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
