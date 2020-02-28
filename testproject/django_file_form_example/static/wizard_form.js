6d44c03ded734df61f8b85329963fc938ced2fc3
Marco Braak <marco@invoicefinance.nl>
2020-02-28 14:30:19 +0100

    Change wizard example

--- testproject/django_file_form_example/static/wizard_form.js
+++ testproject/django_file_form_example/static/wizard_form.js
@@ -4,6 +4,6 @@ const step = form.querySelector(`[name="wizard_exampleview-current_step"]`).valu
 if (step === "0") {
   initUploadFields(
       form,
-      { prefix: step, skipRequired: true }
+      { prefix: step, skipRequired: true, supportDropArea: true }
   );
 }
