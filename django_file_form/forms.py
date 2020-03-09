920fb506437ca222344f6383a94f03a80511007c
Marco Braak <marco@invoicefinance.nl>
2020-03-09 15:25:09 +0100

    Put placeholder files in hidden input

--- django_file_form/forms.py
+++ django_file_form/forms.py
@@ -1,3 +1,4 @@
+import json
 import uuid
 
 from django.urls import reverse
@@ -5,6 +6,7 @@ from django.forms import CharField, HiddenInput
 
 # UploadedFileField and MultipleUploadedFileField must be in this module because they are in the api
 from .fields import UploadedFileField, MultipleUploadedFileField
+from .util import get_list
 
 
 class FileFormMixin(object):
@@ -13,6 +15,8 @@ class FileFormMixin(object):
 
         self.add_hidden_field('form_id', uuid.uuid4())
         self.add_hidden_field('upload_url', self.get_upload_url())
+
+        self.add_placeholder_inputs()
 
     def add_hidden_field(self, name, initial):
         self.fields[name] = CharField(widget=HiddenInput, initial=initial, required=False)
@@ -31,21 +35,27 @@ class FileFormMixin(object):
             # Call super
             super(FileFormMixin, self).full_clean()
 
+    def file_form_field_names(self):
+        return [
+            field_name for field_name, field in self.fields.items()
+            if hasattr(field, 'get_file_data')
+        ]
+
     def update_files_data(self):
         form_id = self.data.get(self.add_prefix('form_id'))
 
         if form_id:
-            for field_name, field in self.fields.items():
-                if hasattr(field, 'get_file_data'):
-                    prefixed_field_name = self.add_prefix(field_name)
-                    file_data = field.get_file_data(prefixed_field_name, form_id)
+            for field_name in self.file_form_field_names():
+                field = self.fields[field_name]
+                prefixed_field_name = self.add_prefix(field_name)
+                file_data = field.get_file_data(prefixed_field_name, form_id)
 
-                    if file_data:
-                        # NB: django-formtools wizard uses dict instead of MultiValueDict
-                        if isinstance(file_data, list) and hasattr(self.files, 'setlist'):
-                            self.files.setlist(prefixed_field_name, file_data)
-                        else:
-                            self.files[prefixed_field_name] = file_data
+                if file_data:
+                    # NB: django-formtools wizard uses dict instead of MultiValueDict
+                    if isinstance(file_data, list) and hasattr(self.files, 'setlist'):
+                        self.files.setlist(prefixed_field_name, file_data)
+                    else:
+                        self.files[prefixed_field_name] = file_data
 
     def delete_temporary_files(self):
         form_id = self.data.get(self.add_prefix('form_id'))
@@ -55,3 +65,16 @@ class FileFormMixin(object):
                 if hasattr(field, 'delete_file_data'):
                     prefixed_field_name = self.add_prefix(field_name)
                     field.delete_file_data(prefixed_field_name, form_id)
+
+    def add_placeholder_inputs(self):
+        for field_name in self.file_form_field_names():
+            placeholder_field_name = f'placeholder-{field_name}'
+            self.add_hidden_field(
+                placeholder_field_name,
+                json.dumps(self.get_placesholders_for_field(field_name))
+            )
+
+    def get_placesholders_for_field(self, field_name):
+        initial_values = get_list(self.initial.get(field_name, []))
+
+        return [value.get_values() for value in initial_values if getattr(value, 'is_placeholder')]
