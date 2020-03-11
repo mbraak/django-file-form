abbfc6d3bf39cff2ec9f5a8e8879860224dcf2d4
Marco Braak <marco@invoicefinance.nl>
2020-03-11 14:27:45 +0100

    Test submit with placeholder

--- testproject/django_file_form_example/tests/test_live.py
+++ testproject/django_file_form_example/tests/test_live.py
@@ -495,11 +495,25 @@ class LiveTestCase(BaseLiveTestCase):
         el.find_element_by_xpath("//*[contains(text(), 'test_placeholder2.txt')]")
         el.find_element_by_xpath("//*[contains(text(), '2 KB')]")
 
+        # add file
+        temp_file = page.create_temp_file('content1')
+        page.upload_using_js(temp_file)
+        page.find_upload_success(temp_file, upload_index=1)
+
         # submit
         page.fill_title_field('abc')
         page.submit()
 
         page.assert_page_contains_text('Upload success')
+
+        example = Example2.objects.get(title='abc')
+        self.assertEqual(example.files.count(), 1)
+
+        example_file = example.files.first()
+        self.assertEqual(example_file.input_file.name, f'example/{temp_file.base_name()}')
+        self.assertEqual(read_file(example_file.input_file), b'content1')
+
+        self.assertEqual(UploadedFile.objects.count(), 0)
 
     def test_delete_placeholder_file(self):
         page = self.page
@@ -512,12 +526,20 @@ class LiveTestCase(BaseLiveTestCase):
         page.delete_ajax_file()
         page.wait_until_upload_is_removed()
 
+        # upload file
+        temp_file = page.create_temp_file('content1')
+        page.upload_using_js(temp_file)
+        page.find_upload_success(temp_file, upload_index=1)
+
         # submit form
         page.submit()
         page.assert_page_contains_text('Title field is required')
 
-        self.assertEqual(len(page.selenium.find_elements_by_css_selector('.dff-files .dff-file')), 1)
+        self.assertEqual(len(page.selenium.find_elements_by_css_selector('.dff-files .dff-file')), 2)
 
         el = page.selenium.find_element_by_css_selector('.dff-file-id-0.dff-upload-success')
         el.find_element_by_xpath("//*[contains(text(), 'test_placeholder2.txt')]")
         el.find_element_by_xpath("//*[contains(text(), '2 KB')]")
+
+        el = page.selenium.find_element_by_css_selector('.dff-file-id-1.dff-upload-success')
+        el.find_element_by_xpath(f"//*[contains(text(), '{temp_file.base_name()}')]")
