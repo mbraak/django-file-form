2fc50a333ef0dcd07f53db9b241488ad8d6c920a
Marco Braak <marco@invoicefinance.nl>
2020-05-22 10:31:31 +0200

    Use new urls syntax

--- django_file_form/urls.py
+++ django_file_form/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import url
+from django.urls import path
 from .views.tus import TusUpload
 
 
 urlpatterns = (
-    url(r'^upload/$', TusUpload.as_view(), name='tus_upload'),
-    url(r'^upload/(?P<resource_id>[0-9a-z-]+)$', TusUpload.as_view(), name='tus_upload_chunks'),
+    path('upload/', TusUpload.as_view(), name='tus_upload'),
+    path('upload/<str:resource_id>', TusUpload.as_view(), name='tus_upload_chunks'),
 )
