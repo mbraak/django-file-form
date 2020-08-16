5c2c2bac5aa38127bdbd5779fe7cf08b58600130
Bo Peng <ben.bog@gmail.com>
2020-08-16 13:23:56 -0500

    Replace existing uploaded file with the same name mbraak/django-file-form#323

--- frontend/src/upload_file.ts
+++ frontend/src/upload_file.ts
@@ -155,7 +155,33 @@ class UploadFile {
     files.forEach(file => {
       const { fieldName, formId, renderer, uploads, uploadUrl } = this;
       const filename = file.name;
-      const uploadIndex = uploads.length;
+      let uploadIndex = uploads.length;
+
+      // #323 remove existing file
+      for (let index = 0; index < this.uploads.length; ++index) {
+        let existing_upload = this.uploads[index];
+        if (existing_upload instanceof Upload) {
+          if (existing_upload.file.name == filename) {
+            uploadIndex = index;
+            const el = this.renderer.findFileDiv(index);
+            if (el && el.classList.contains('dff-upload-fail')) {
+              // a failed one, will try to resume
+              this.deleteUpload(index);
+              break;
+            } else {
+              // a successful upload
+              this.deleteFromServer(index);
+              break;
+            }
+          }
+        } else if (existing_upload) {
+          if (existing_upload.name === filename) {
+            this.deletePlaceholder(index);
+            uploadIndex = index;
+            break;
+          }
+        }
+      }
 
       const upload = new Upload(file, {
         endpoint: uploadUrl,
@@ -171,7 +197,11 @@ class UploadFile {
       upload.start();
       renderer.addNewUpload(filename, uploadIndex);
 
-      this.uploads.push(upload);
+      if (uploadIndex == this.uploads.length) {
+        this.uploads.push(upload);
+      } else {
+        this.uploads[uploadIndex] = upload;
+      }
     });
 
     this.checkDropHint();
