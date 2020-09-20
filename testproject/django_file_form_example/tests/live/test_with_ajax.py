5f2a48b86e366b5e90d8db80c83548303772f7b7
Marco Braak <marco@invoicefinance.nl>
2020-09-20 22:00:50 +0200

    Split tests

--- testproject/django_file_form_example/tests/live/test_with_ajax.py
+++ testproject/django_file_form_example/tests/live/test_with_ajax.py
@@ -16,50 +16,6 @@ media_root = Path(settings.MEDIA_ROOT)
 class LiveTestCase(BaseLiveTestCase):
     page_class = Page
 
-    def test_submit_single_without_ajax(self):
-        page = self.page
-
-        temp_file = page.create_temp_file(content='content')
-
-        page.open('/without_js')
-        page.fill_title_field('xyz')
-        page.upload_without_js(temp_file)
-        page.submit()
-
-        page.assert_page_contains_text('Upload success')
-
-        self.assertEqual(temp_file.uploaded_file().read_text(), "content")
-
-        example = Example.objects.get(title='xyz')
-        self.assertEqual(example.input_file.name, f'example/{temp_file.base_name()}')
-
-    def test_submit_multiple_without_ajax(self):
-        page = self.page
-
-        temp_file1 = page.create_temp_file('content1')
-        temp_file2 = page.create_temp_file('content2')
-
-        page.open('/multiple_without_js')
-        page.fill_title_field('abc')
-        page.upload_without_js(temp_file1)
-        page.upload_without_js(temp_file2)
-        page.submit()
-
-        page.assert_page_contains_text('Upload success')
-
-        self.assertEqual(temp_file1.uploaded_file().read_text(), "content1")
-        self.assertEqual(temp_file2.uploaded_file().read_text(), "content2")
-
-        self.assertEqual(Example2.objects.count(), 1)
-
-        example2 = Example2.objects.first()
-        self.assertEqual(example2.title, 'abc')
-
-        self.assertSetEqual(
-            {f.input_file.name for f in example2.files.all()},
-            {f'example/{temp_file1.base_name()}', f'example/{temp_file2.base_name()}'}
-        )
-
     def test_upload(self):
         page = self.page
 
@@ -97,7 +53,7 @@ class LiveTestCase(BaseLiveTestCase):
         self.assertFalse(Path(uploaded_file.uploaded_file.path).exists())
 
     def test_upload_binary_file(self):
-        content = Path(__file__).parent.joinpath('test_data/wikipedia.png').read_bytes()
+        content = Path(__file__).parent.joinpath('../test_data/wikipedia.png').read_bytes()
 
         page = self.page
         temp_file = page.create_temp_file(content, binary=True)
---
+++ testproject/django_file_form_example/tests/live/test_without_ajax.py
@@ -0,0 +1,51 @@
+from django_file_form_example.base_live_testcase import BaseLiveTestCase
+from django_file_form_example.page import Page
+from django_file_form_example.models import Example, Example2
+
+
+class LiveTestCase(BaseLiveTestCase):
+    page_class = Page
+
+    def test_submit_single(self):
+        page = self.page
+
+        temp_file = page.create_temp_file(content='content')
+
+        page.open('/without_js')
+        page.fill_title_field('xyz')
+        page.upload_without_js(temp_file)
+        page.submit()
+
+        page.assert_page_contains_text('Upload success')
+
+        self.assertEqual(temp_file.uploaded_file().read_text(), "content")
+
+        example = Example.objects.get(title='xyz')
+        self.assertEqual(example.input_file.name, f'example/{temp_file.base_name()}')
+
+    def test_submit_multiple(self):
+        page = self.page
+
+        temp_file1 = page.create_temp_file('content1')
+        temp_file2 = page.create_temp_file('content2')
+
+        page.open('/multiple_without_js')
+        page.fill_title_field('abc')
+        page.upload_without_js(temp_file1)
+        page.upload_without_js(temp_file2)
+        page.submit()
+
+        page.assert_page_contains_text('Upload success')
+
+        self.assertEqual(temp_file1.uploaded_file().read_text(), "content1")
+        self.assertEqual(temp_file2.uploaded_file().read_text(), "content2")
+
+        self.assertEqual(Example2.objects.count(), 1)
+
+        example2 = Example2.objects.first()
+        self.assertEqual(example2.title, 'abc')
+
+        self.assertSetEqual(
+            {f.input_file.name for f in example2.files.all()},
+            {f'example/{temp_file1.base_name()}', f'example/{temp_file2.base_name()}'}
+        )
