556359f5edacf55cf66176f4ff3c29dcd213db67
Marco Braak <marco.braak@planet.nl>
2020-10-24 15:26:53 +0200

    Refactor s3 views (#374)
    * Use a module for the s3 views
    
    * Add s3 and tus packages
    
    * Extract s3 utils
    
    * Extract s3 urls
    
    * Extract tus urls
    
    * Add missing file
    
    * Extract tus utils
    
    * Add StartTusUpload view
    
    * Use require_POST and other decorators for s3 views
    
    * Add logging to s3 views
    
    * Refactor s3 views
    
    * Make 'start upload' view a method
    
    * Use methods for tus views

---
+++ django_file_form/tus/utils.py
@@ -0,0 +1,57 @@
+import os
+from django.conf import settings
+from django.core.cache import caches
+from django.core.files import File
+from django.http import HttpResponse
+
+from django_file_form.models import UploadedFile
+from django_file_form import conf
+
+
+cache = caches[getattr(settings, 'FILE_FORM_CACHE', 'default')]
+
+
+def remove_resource_from_cache(resource_id):
+    cache.delete_many([
+        "tus-uploads/{}/file_size".format(resource_id),
+        "tus-uploads/{}/filename".format(resource_id),
+        "tus-uploads/{}/offset".format(resource_id),
+        "tus-uploads/{}/metadata".format(resource_id),
+    ])
+
+
+def create_uploaded_file_in_db(field_name, file_id, form_id, original_filename, uploaded_file):
+    with open(uploaded_file, 'rb') as fh:
+        values = dict(
+            file_id=file_id,
+            form_id=form_id,
+            uploaded_file=File(file=fh, name=uploaded_file.name),
+            original_filename=original_filename
+        )
+
+        if field_name:
+            values['field_name'] = field_name
+
+        UploadedFile.objects.create(**values)
+
+    os.remove(uploaded_file)
+
+
+tus_api_version = '1.0.0'
+tus_api_version_supported = ['1.0.0', ]
+tus_api_extensions = ['creation', 'termination', 'file-check']
+
+
+def get_tus_response():
+    response = HttpResponse()
+    response['Tus-Resumable'] = tus_api_version
+    response['Tus-Version'] = ",".join(tus_api_version_supported)
+    response['Tus-Extension'] = ",".join(tus_api_extensions)
+    response['Tus-Max-Size'] = conf.MAX_FILE_SIZE
+    response['Access-Control-Allow-Origin'] = "*"
+    response['Access-Control-Allow-Methods'] = "PATCH,HEAD,GET,POST,OPTIONS"
+    response['Access-Control-Expose-Headers'] = "Tus-Resumable,upload-length,upload-metadata,Location,Upload-Offset"
+    response['Access-Control-Allow-Headers'] = "Tus-Resumable,upload-length,upload-metadata,Location,Upload-Offset,content-type"
+    response['Cache-Control'] = 'no-store'
+
+    return response
--- django_file_form/urls.py
+++ django_file_form/urls.py
@@ -1,20 +1,6 @@
-from django.urls import path
-from .views.tus import TusUpload
+from django.urls import include, path
 
-tus_patterns = [
-    path('upload/', TusUpload.as_view(), name='tus_upload'),
-    path('upload/<str:resource_id>', TusUpload.as_view(), name='tus_upload_chunks')
+urlpatterns = [
+    path('upload/', include('django_file_form.tus.urls')),
+    path('s3upload/', include('django_file_form.s3_multipart.urls'))
 ]
-
-try:
-    from .views.s3multipart import s3multipart
-    s3_patterns = [
-        path('s3upload/', s3multipart.create_multipart_upload, name='s3_upload'),
-        path('s3upload/<upload_id>/', s3multipart.get_parts_or_abort_upload, name='get_parts_or_abort_upload'),
-        path('s3upload/<upload_id>/<int:part_number>', s3multipart.sign_part_upload, name='sign_part_upload'),
-        path('s3upload/<upload_id>/complete', s3multipart.complete_multipart_upload, name='complete_multipart_upload'),
-    ]
-except ImportError:
-    s3_patterns = []
-
-urlpatterns = tus_patterns + s3_patterns
