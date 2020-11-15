f4d7061e416e1048f0fb97ec1b47c1f737853d3c
Marco Braak <marco@invoicefinance.nl>
2020-11-15 11:13:06 +0100

    Extract UploadInProgress

--- frontend/src/renderUploads/Upload.tsx
+++ frontend/src/renderUploads/Upload.tsx
@@ -1,60 +1,7 @@
 import BaseUpload from "../uploads/base_upload";
 import { formatBytes } from "../util";
 import { Translations } from "./types";
-
-interface CancelLinkProps {
-  onDelete: (upload: BaseUpload) => void;
-  translations: Translations;
-  upload: BaseUpload;
-}
-
-const CancelLink = ({ onDelete, translations, upload }: CancelLinkProps) => {
-  const handleCancel = () => onDelete(upload);
-
-  const classes = ["dff-cancel"];
-  const cancelling = upload.deleteStatus === "in_progress";
-
-  if (cancelling) {
-    classes.push("dff-disabled");
-  }
-
-  return (
-    <a
-      className="dff-cancel"
-      href="#"
-      onClick={cancelling ? undefined : handleCancel}
-    >
-      {translations.Cancel}
-    </a>
-  );
-};
-
-interface UploadInProgressProps {
-  onDelete: (upload: BaseUpload) => void;
-  translations: Translations;
-  upload: BaseUpload;
-}
-
-const UploadInProgress = ({
-  onDelete,
-  translations,
-  upload
-}: UploadInProgressProps) => (
-  <div className={`dff-file dff-file-id-${upload.uploadIndex}`}>
-    <span>{upload.name}</span>
-    <span className="dff-progress">
-      <span
-        className="dff-progress-inner"
-        style={{ width: `${upload.progress.toFixed(2)}%` }}
-      />
-    </span>
-    <CancelLink
-      onDelete={onDelete}
-      translations={translations}
-      upload={upload}
-    />
-  </div>
-);
+import UploadInProgress from "./UploadInProgress";
 
 interface DeleteLinkProps {
   onDelete: (upload: BaseUpload) => void;
