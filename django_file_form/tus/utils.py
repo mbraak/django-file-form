0a5902932cce563eb026041ed7d8c7ecd1284f96
Marco Braak <marco@invoicefinance.nl>
2020-11-19 22:09:29 +0100

    Skip file copy if possible

--- django_file_form/tus/utils.py
+++ django_file_form/tus/utils.py
@@ -1,10 +1,12 @@
 import os
+from pathlib import Path
 from django.conf import settings
 from django.core.cache import caches
 from django.core.files import File
 from django.http import HttpResponse
+from django.core.files.storage import FileSystemStorage
 
-from django_file_form.models import TemporaryUploadedFile
+from django_file_form.models import TemporaryUploadedFile, storage_class
 from django_file_form import conf
 
 
@@ -25,6 +27,19 @@ def remove_resource_from_cache(resource_id):
 def create_uploaded_file_in_db(
     field_name, file_id, form_id, original_filename, uploaded_file
 ):
+    if storage_class == FileSystemStorage:
+        create_uploaded_file_in_db_default(
+            field_name, file_id, form_id, original_filename, uploaded_file
+        )
+    else:
+        create_uploaded_file_in_db_non_default(
+            field_name, file_id, form_id, original_filename, uploaded_file
+        )
+
+
+def create_uploaded_file_in_db_non_default(
+    field_name, file_id, form_id, original_filename, uploaded_file
+):
     with open(uploaded_file, "rb") as fh:
         values = dict(
             file_id=file_id,
@@ -39,6 +54,22 @@ def create_uploaded_file_in_db(
         TemporaryUploadedFile.objects.create(**values)
 
     os.remove(uploaded_file)
+
+
+def create_uploaded_file_in_db_default(
+    field_name, file_id, form_id, original_filename, uploaded_file
+):
+    values = dict(
+        file_id=file_id,
+        form_id=form_id,
+        uploaded_file=str(uploaded_file.relative_to(Path(settings.MEDIA_ROOT))),
+        original_filename=original_filename
+    )
+
+    if field_name:
+        values['field_name'] = field_name
+
+    TemporaryUploadedFile.objects.create(**values)
 
 
 tus_api_version = "1.0.0"
