b21de07e8541ab0dfe6da82ad25d70a1d4737434
Marco Braak <marco@invoicefinance.nl>
2020-12-25 13:00:12 +0100

    Render custom input

--- testproject/frontend/src/example_form_custom_widget.js
+++ testproject/frontend/src/example_form_custom_widget.js
@@ -1,48 +1,21 @@
-import EventEmitter from "eventemitter3";
-
 const { formatBytes } = window.djangoFileForm;
 
-const eventEmitter = new EventEmitter();
-
-eventEmitter.on("addUpload", ({ element, fieldName, fileName, metaDataField, upload }) => {
-  const descriptionChanged = (evt) => {
-    const metaData = JSON.parse(metaDataField.value);
-
-    const inputValue = evt.target.value;
-    metaData[fileName] = { description: inputValue };
-
-    metaDataField.value = JSON.stringify(metaData);
+const CustomFileInfo = ({ upload }) => {
+  const handleDescriptionChange = (e) => {
+    upload.metadata.description = e.target.value;
   };
 
-  if (!metaDataField || !metaDataField.value) {
-    return;
-  }
-
-  const metadata = JSON.parse(metaDataField.value);
-
-  // add a widget
-  const descElem = document.createElement("input");
-  descElem.value = metadata[fileName] ? metadata[fileName]["description"] : "";
-
-  descElem.className = "dff-description";
-  descElem.addEventListener("change", descriptionChanged);
-  element.insertBefore(descElem, element.firstElementChild.nextElementSibling);
-});
-
-eventEmitter.on("removeUpload", ({ element, fieldName, fileName, metaDataField, upload }) => {
-  // do not need to update hidden data since returned metadata will be ignored
-});
-
-const CustomFileInfo = ({ upload }) => (
-  <>
-    <span>{upload.name}</span>
-    <span className="dff-filesize">{formatBytes(upload.getSize(), 2)}</span>
-  </>
-);
+  return (
+    <>
+      <span>{upload.name}</span>
+      <span className="dff-filesize">{formatBytes(upload.getSize(), 2)}</span>
+      <input className="dff-description" onInput={handleDescriptionChange} value={upload?.metadata?.description} />
+    </>
+  );
+};
 
 initUploadFields(document.getElementById("example-form"), {
   CustomFileInfo,
-  eventEmitter,
   prefix: "example",
   retryDelays: [],
   skipRequired: true,
