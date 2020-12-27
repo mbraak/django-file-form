c722cd5812555d56b3a44bca1a80f24aaa299c5e
requires.io <support@requires.io>
2020-12-27 13:47:13 +0100

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-version = '3.1.1'
+version = '3.1.2'
 
 setup(
     name='django-file-form',
@@ -33,6 +33,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: Implementation :: CPython"
     ]
 )
