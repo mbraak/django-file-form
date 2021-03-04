07abff765c5ad4d8d8a0bccb27ff7afac9252046
Marco Braak <marco@invoicefinance.nl>
2021-03-04 16:34:30 +0100

    Add FileFormAdminMixin

--- django_file_form/admin.py
+++ django_file_form/admin.py
@@ -1,6 +1,31 @@
+from django.db import models
 from django.contrib import admin
+from django.forms import ModelForm
 
-from . import models
+from .models import TemporaryUploadedFile
+from .fields import UploadedFileField
+from .forms import FileFormMixin
+from .widgets import UploadWidget
+
+
+class FileAdminForm(FileFormMixin, ModelForm):
+    pass
+
+
+class FileFormAdminMixin:
+    change_form_template = 'django_file_form/admin_change_form.html'
+    form = FileAdminForm
+    formfield_overrides = {
+        models.FileField: {
+            'form_class': UploadedFileField,
+            'widget': UploadWidget,
+        }
+    }
+
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+
+        form.delete_temporary_files()
 
 
 class TemporaryUploadedFileAdmin(admin.ModelAdmin):
@@ -8,4 +33,4 @@ class TemporaryUploadedFileAdmin(admin.ModelAdmin):
     date_hierarchy = "created"
 
 
-admin.site.register(models.TemporaryUploadedFile, TemporaryUploadedFileAdmin)
+admin.site.register(TemporaryUploadedFile, TemporaryUploadedFileAdmin)
--- testproject/django_file_form_example/admin.py
+++ testproject/django_file_form_example/admin.py
@@ -1,40 +1,15 @@
 from django.contrib import admin
-from django.db import models
-from django.forms import ModelForm
 
-from django_file_form.fields import UploadedFileField
-from django_file_form.forms import FileFormMixin
-from django_file_form.widgets import UploadWidget
+from django_file_form.admin import FileFormAdminMixin
 from .models import Example, Example2, ExampleFile
 
 
-class ExampleAdminForm(FileFormMixin, ModelForm):
-    class Meta:
-        model = Example
-        fields = "__all__"
-
-
-class ExampleAdmin(admin.ModelAdmin):
+class ExampleAdmin(FileFormAdminMixin, admin.ModelAdmin):
     class Media:
         css = {
             "all": ("file_form/file_form.css",)
         }
         js = ("file_form/file_form.js", "example_admin.js")
-
-    change_form_template = 'django_file_form/admin_change_form.html'
-    form = ExampleAdminForm
-
-    formfield_overrides = {
-        models.FileField: {
-            'form_class': UploadedFileField,
-            'widget': UploadWidget,
-        }
-    }
-
-    def save_model(self, request, obj, form, change):
-        super().save_model(request, obj, form, change)
-
-        form.delete_temporary_files()
 
 
 class ExampleFileInline(admin.TabularInline):
