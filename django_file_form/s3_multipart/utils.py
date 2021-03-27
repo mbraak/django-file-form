88297484a6fbeaa469dd677e5907ffbebb3d6b5d
Marco Braak <marco@invoicefinance.nl>
2021-03-27 09:47:31 +0100

    Add AWS_SIGNATURE_VERSION option

--- django_file_form/s3_multipart/utils.py
+++ django_file_form/s3_multipart/utils.py
@@ -3,6 +3,7 @@ import time
 
 import boto3
 from botocore.exceptions import ClientError
+from botocore.client import Config
 from django.utils.crypto import get_random_string
 from storages.utils import setting, lookup_env
 
@@ -36,6 +37,8 @@ def file_form_upload_dir():
 
 
 def get_client():
+    signature_version = setting("AWS_SIGNATURE_VERSION", None)
+
     while True:
         try:
             # https://github.com/boto/boto3/issues/801
@@ -44,6 +47,7 @@ def get_client():
                 endpoint_url=get_endpoint_url(),
                 aws_access_key_id=get_access_key_id(),
                 aws_secret_access_key=get_secret_access_key(),
+                config=Config(signature_version=signature_version)
             )
         except:
             time.sleep(0.01)
