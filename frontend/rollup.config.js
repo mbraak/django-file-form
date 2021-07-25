97c03541ddad95e1b59c976886431620ccd8643f
Marco Braak <marco@invoicefinance.nl>
2021-07-25 08:32:07 +0200

    Use pnpm

--- frontend/rollup.config.js
+++ frontend/rollup.config.js
@@ -25,7 +25,7 @@ const plugins = [
   babel({
     babelHelpers: "runtime",
     extensions: [".js", ".ts"],
-    exclude: "node_modules/core-js/**"
+    exclude: ["node_modules/core-js/**", "node_modules/**/core-js*/**"]
   })
 ];
 
