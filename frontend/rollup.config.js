00b56a702a874233bac794a1a0c8d944d6f2ab16
Marco Braak <marco@invoicefinance.nl>
2021-07-25 09:44:00 +0200

    Use babel-plugin-istanbul

--- frontend/rollup.config.js
+++ frontend/rollup.config.js
@@ -1,13 +1,11 @@
+import path from "path";
 import babel from "@rollup/plugin-babel";
 import commonjs from "@rollup/plugin-commonjs";
-import coverage from "rollup-plugin-istanbul2";
 import resolve from "@rollup/plugin-node-resolve";
 import { terser } from "rollup-plugin-terser";
 
 const skipCompressJs = Boolean(process.env.SKIP_COMPRESS_JS);
 const includeCoverage = Boolean(process.env.COVERAGE);
-
-const minimize = !skipCompressJs && !includeCoverage;
 
 const getOutputFilename = () => {
   if (includeCoverage) {
@@ -19,11 +17,17 @@ const getOutputFilename = () => {
   }
 };
 
+const minimize = !skipCompressJs && !includeCoverage;
+const babelConfigFile = includeCoverage
+  ? "babel.coverage.config.json"
+  : "babel.config.json";
+
 const plugins = [
   resolve({ browser: true, extensions: [".js", ".ts"] }),
   commonjs(),
   babel({
     babelHelpers: "runtime",
+    configFile: path.resolve(__dirname, babelConfigFile),
     extensions: [".js", ".ts"],
     exclude: ["node_modules/core-js/**", "node_modules/**/core-js*/**"]
   })
@@ -32,13 +36,6 @@ const plugins = [
 if (minimize) {
   const terserPlugin = terser();
   plugins.push(terserPlugin);
-}
-
-if (includeCoverage) {
-  const coveragePlugin = coverage({
-    exclude: ["node_modules/**"]
-  });
-  plugins.push(coveragePlugin);
 }
 
 export default {
