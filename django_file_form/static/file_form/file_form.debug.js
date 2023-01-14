b39a8d5ec70e7ca2fe50031797b6b2a305618b2b
Marco Braak <marco@floryn.com>
2023-01-14 13:15:39 +0100

    Update js packages (#554)
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update js
    
    * Update

--- django_file_form/static/file_form/file_form.debug.js
+++ django_file_form/static/file_form/file_form.debug.js
@@ -20,7 +20,34 @@
     forms.forEach(initUploadFields);
   };
 
+  function _typeof$2(obj) {
+    "@babel/helpers - typeof";
+
+    return _typeof$2 = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) {
+      return typeof obj;
+    } : function (obj) {
+      return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj;
+    }, _typeof$2(obj);
+  }
+
+  function _toPrimitive(input, hint) {
+    if (_typeof$2(input) !== "object" || input === null) return input;
+    var prim = input[Symbol.toPrimitive];
+    if (prim !== undefined) {
+      var res = prim.call(input, hint || "default");
+      if (_typeof$2(res) !== "object") return res;
+      throw new TypeError("@@toPrimitive must return a primitive value.");
+    }
+    return (hint === "string" ? String : Number)(input);
+  }
+
+  function _toPropertyKey(arg) {
+    var key = _toPrimitive(arg, "string");
+    return _typeof$2(key) === "symbol" ? key : String(key);
+  }
+
   function _defineProperty$2(obj, key, value) {
+    key = _toPropertyKey(key);
     if (key in obj) {
       Object.defineProperty(obj, key, {
         value: value,
@@ -57,10 +84,7 @@
     return input;
   };
   const getUploadsFieldName = (fieldName, prefix) => `${getInputNameWithoutPrefix(fieldName, prefix)}-uploads`;
-  const getInputValueForFormAndPrefix = (form, fieldName, prefix) => {
-    var _findInput;
-    return (_findInput = findInput(form, fieldName, prefix)) === null || _findInput === void 0 ? void 0 : _findInput.value;
-  };
+  const getInputValueForFormAndPrefix = (form, fieldName, prefix) => findInput(form, fieldName, prefix)?.value;
   const getMetadataFieldName = (fieldName, prefix) => `${getInputNameWithoutPrefix(fieldName, prefix)}-metadata`;
 
   var commonjsGlobal = typeof globalThis !== 'undefined' ? globalThis : typeof window !== 'undefined' ? window : typeof global !== 'undefined' ? global : typeof self !== 'undefined' ? self : {};
@@ -747,7 +771,11 @@
   let Mime = Mime_1;
   var lite = new Mime(standard);
 
-  var picomatch$2 = {exports: {}};
+  var picomatchExports = {};
+  var picomatch$2 = {
+    get exports(){ return picomatchExports; },
+    set exports(v){ picomatchExports = v; },
+  };
 
   var check = function (it) {
     return it && it.Math == Math && it;
@@ -780,11 +808,16 @@
     return Object.defineProperty({}, 1, { get: function () { return 7; } })[1] != 7;
   });
 
-  var makeBuiltIn$2 = {exports: {}};
+  var makeBuiltInExports = {};
+  var makeBuiltIn$2 = {
+    get exports(){ return makeBuiltInExports; },
+    set exports(v){ makeBuiltInExports = v; },
+  };
 
   var documentAll$2 = typeof document == 'object' && document.all;
 
   // https://tc39.es/ecma262/#sec-IsHTMLDDA-internal-slot
+  // eslint-disable-next-line unicorn/no-typeof-undefined -- required for testing
   var IS_HTMLDDA = typeof documentAll$2 == 'undefined' && documentAll$2 !== undefined;
 
   var documentAll_1 = {
@@ -819,29 +852,10 @@
   var call$3 = FunctionPrototype$1.call;
   var uncurryThisWithBind = NATIVE_BIND$1 && FunctionPrototype$1.bind.bind(call$3, call$3);
 
-  var functionUncurryThisRaw = NATIVE_BIND$1 ? uncurryThisWithBind : function (fn) {
+  var functionUncurryThis = NATIVE_BIND$1 ? uncurryThisWithBind : function (fn) {
     return function () {
       return call$3.apply(fn, arguments);
     };
-  };
-
-  var uncurryThisRaw$1 = functionUncurryThisRaw;
-
-  var toString$2 = uncurryThisRaw$1({}.toString);
-  var stringSlice = uncurryThisRaw$1(''.slice);
-
-  var classofRaw$1 = function (it) {
-    return stringSlice(toString$2(it), 8, -1);
-  };
-
-  var classofRaw = classofRaw$1;
-  var uncurryThisRaw = functionUncurryThisRaw;
-
-  var functionUncurryThis = function (fn) {
-    // Nashorn bug:
-    //   https://github.com/zloirock/core-js/issues/1128
-    //   https://github.com/zloirock/core-js/issues/1130
-    if (classofRaw(fn) === 'Function') return uncurryThisRaw(fn);
   };
 
   // we can't use just `it == null` since of `document.all` special case
@@ -1143,17 +1157,21 @@
     throw $TypeError$2("Can't convert object to primitive value");
   };
 
-  var shared$3 = {exports: {}};
+  var sharedExports = {};
+  var shared$3 = {
+    get exports(){ return sharedExports; },
+    set exports(v){ sharedExports = v; },
+  };
 
   var store$1 = sharedStore;
 
   (shared$3.exports = function (key, value) {
     return store$1[key] || (store$1[key] = value !== undefined ? value : {});
   })('versions', []).push({
-    version: '3.26.0',
+    version: '3.27.1',
     mode: 'global',
     copyright: '© 2014-2022 Denis Pushkarev (zloirock.ru)',
-    license: 'https://github.com/zloirock/core-js/blob/v3.26.0/LICENSE',
+    license: 'https://github.com/zloirock/core-js/blob/v3.27.1/LICENSE',
     source: 'https://github.com/zloirock/core-js'
   });
 
@@ -1168,7 +1186,7 @@
   };
 
   var global$3 = global$a;
-  var shared$2 = shared$3.exports;
+  var shared$2 = sharedExports;
   var hasOwn$2 = hasOwnProperty_1;
   var uid$1 = uid$2;
   var NATIVE_SYMBOL = symbolConstructorDetection;
@@ -1292,7 +1310,7 @@
     return object;
   };
 
-  var shared$1 = shared$3.exports;
+  var shared$1 = sharedExports;
   var uid = uid$2;
 
   var keys = shared$1('keys');
@@ -1420,7 +1438,7 @@
     return isCallable(this) && getInternalState(this).source || inspectSource(this);
   }, 'toString');
 
-  var makeBuiltIn = makeBuiltIn$2.exports;
+  var makeBuiltIn = makeBuiltInExports;
   var defineProperty = objectDefineProperty;
 
   var defineBuiltInAccessor$1 = function (target, name, descriptor) {
@@ -3624,7 +3642,7 @@
 
     module.exports = picomatch_1;
   })(picomatch$2);
-  var picomatch = /*@__PURE__*/getDefaultExportFromCjs(picomatch$2.exports);
+  var picomatch = /*@__PURE__*/getDefaultExportFromCjs(picomatchExports);
 
   const parseInputAccept = inputAccept => {
     const extensions = [];
@@ -4376,7 +4394,7 @@
    *
    * @author Dan Kogai (https://github.com/dankogai)
    */
-  const version = '3.7.2';
+  const version = '3.7.4';
   /**
    * @deprecated use lowercase `version`.
    */
@@ -7722,7 +7740,6 @@
   }
 
   const initUploadFields = function (form) {
-    var _findInput;
     let options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
     const matchesPrefix = fieldName => {
       if (!(options && options.prefix)) {
@@ -7744,7 +7761,7 @@
     const s3UploadDir = getInputValue("s3_upload_dir");
     const skipRequired = options.skipRequired || false;
     const prefix = getPrefix();
-    const csrfToken = (_findInput = findInput(form, "csrfmiddlewaretoken", null)) === null || _findInput === void 0 ? void 0 : _findInput.value;
+    const csrfToken = findInput(form, "csrfmiddlewaretoken", null)?.value;
     if (!csrfToken) {
       throw Error("Csrf token not found");
     }
