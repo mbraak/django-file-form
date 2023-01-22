cec38c9f3b5242116f70be8d2762823388ef4b80
Marco Braak <marco@floryn.com>
2023-01-22 11:44:36 +0100

    Call onClick only for finished uploads

--- frontend/src/file_field.ts
+++ frontend/src/file_field.ts
@@ -343,7 +343,7 @@ class FileField {
 
       const upload = getUpload();
 
-      if (upload && this.callbacks.onClick) {
+      if (upload?.status === "done" && this.callbacks.onClick) {
         this.callbacks.onClick({
           fileName: upload.name,
           fieldName: this.fieldName,
