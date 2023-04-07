d04a5b827d7cd138bb9c45d15d553dee509fbeca
Marco Braak <marco@floryn.com>
2023-04-07 09:18:32 +0200

    Build

--- django_file_form/static/file_form/file_form.debug.js
+++ django_file_form/static/file_form/file_form.debug.js
@@ -1166,10 +1166,10 @@
   (shared$3.exports = function (key, value) {
     return store$1[key] || (store$1[key] = value !== undefined ? value : {});
   })('versions', []).push({
-    version: '3.29.1',
+    version: '3.30.0',
     mode: 'global',
     copyright: '© 2014-2023 Denis Pushkarev (zloirock.ru)',
-    license: 'https://github.com/zloirock/core-js/blob/v3.29.1/LICENSE',
+    license: 'https://github.com/zloirock/core-js/blob/v3.30.0/LICENSE',
     source: 'https://github.com/zloirock/core-js'
   });
 
