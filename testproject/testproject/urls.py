c481fce54dc4cbb74abcef32af44780acdceedce
Marco Braak <marco@floryn.com>
2023-06-13 08:10:15 +0200

    Remove custom 403 view

--- testproject/testproject/urls.py
+++ testproject/testproject/urls.py
@@ -2,8 +2,6 @@ from django.urls import path, include
 from django.contrib import admin
 
 
-handler403 = "django_file_form_example.views.permission_denied"
-
 urlpatterns = (
     path("admin/", admin.site.urls),
     path("", include("django_file_form.urls")),
