d22b143605d9164cd1e1ca4f00289c1beef74d88
Marco Braak <marco@floryn.com>
2023-06-15 10:57:46 +0200

    Add test

--- testproject/django_file_form_example/tests/test_views.py
+++ testproject/django_file_form_example/tests/test_views.py
@@ -1,8 +1,16 @@
 import re
-
-from django.test import TestCase, Client
+from django.test import Client, override_settings, TestCase
+from django.core.exceptions import PermissionDenied
 
 from django_file_form_example.tests.utils.test_utils import remove_test_files
+
+
+def custom_check_permissions(request, _field_name):
+    raise PermissionDenied()
+
+
+def custom_check_permissions_with_message(request, _field_name):
+    raise PermissionDenied('test message1')
 
 
 class ViewTests(TestCase):
@@ -36,6 +44,28 @@ class ViewTests(TestCase):
     def test_head_with_unknown_resource_id(self):
         response = Client().head("/upload/unknown")
         self.assertEqual(response.status_code, 404)
+
+    @override_settings(FILE_FORM_CHECK_PERMISSIONS="django_file_form_example.tests.test_views.custom_check_permissions")
+    def test_custom_permissions(self):
+        response = Client().post("/upload/", HTTP_TUS_RESUMABLE=True)
+
+        self.assertEqual(response.status_code, 403)
+        self.assertEqual(
+            response.json(),
+            dict(message=None, status='permission denied')
+        )
+
+    @override_settings(
+        FILE_FORM_CHECK_PERMISSIONS="django_file_form_example.tests.test_views.custom_check_permissions_with_message"
+    )
+    def test_custom_permissions_with_custom_message(self):
+        response = Client().post("/upload/", HTTP_TUS_RESUMABLE=True)
+
+        self.assertEqual(response.status_code, 403)
+        self.assertEqual(
+            response.json(),
+            dict(message='test message1', status='permission denied')
+        )
 
     def do_post(self, client):
         response = client.post("/upload/", HTTP_TUS_RESUMABLE=True)
