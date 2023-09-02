7151138abdea391e9a3e78012d71b30ed265390c
Marco Braak <marco@floryn.com>
2023-09-02 09:39:46 +0200

    Add tests

--- frontend/src/util.test.ts
+++ frontend/src/util.test.ts
@@ -1,4 +1,9 @@
-import { formatBytes } from "./util";
+import {
+  formatBytes,
+  getInputNameWithPrefix,
+  getMetadataFieldName,
+  getUploadsFieldName
+} from "./util";
 
 describe("formatBytes", () => {
   it("formats 0 bytes", () => {
@@ -11,5 +16,39 @@ describe("formatBytes", () => {
 
   it("formats 1200 with 2 decimals", () => {
     expect(formatBytes(1200, 2)).toEqual("1.17 KB");
+  });
+});
+
+describe("getInputNameWithPrefix", () => {
+  it("returns the name with prefix when there is a prefix", () => {
+    expect(getInputNameWithPrefix("test_field", "abc")).toBe("abc-test_field");
+  });
+
+  it("returns the name when there is no prefix", () => {
+    expect(getInputNameWithPrefix("test_field", null)).toBe("test_field");
+  });
+});
+
+describe("getUploadsFieldName", () => {
+  it("returns the name with a suffix when there is a prefix", () => {
+    expect(getUploadsFieldName("field1", "testprefix")).toBe(
+      "testprefix-field1-uploads"
+    );
+  });
+
+  it("returns the name with a suffix when there is no prefix", () => {
+    expect(getUploadsFieldName("field1", null)).toBe("field1-uploads");
+  });
+});
+
+describe("getMetadataFieldName", () => {
+  it("returns the name with a suffix when there is a prefix", () => {
+    expect(getMetadataFieldName("field1", "testprefix")).toBe(
+      "testprefix-field1-metadata"
+    );
+  });
+
+  it("returns the name with a suffix when there is no prefix", () => {
+    expect(getMetadataFieldName("field1", null)).toBe("field1-metadata");
   });
 });
