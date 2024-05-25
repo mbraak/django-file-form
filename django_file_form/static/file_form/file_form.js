42f6018c64e18f006b775e1951f2e4e82f508496
Marco Braak <marco@floryn.com>
2024-05-25 08:50:54 +0200

    Update

--- django_file_form/static/file_form/file_form.js
+++ django_file_form/static/file_form/file_form.js
@@ -46,19 +46,13 @@
     return "symbol" == _typeof(i) ? i : i + "";
   }
 
-  function _defineProperty(obj, key, value) {
-    key = toPropertyKey(key);
-    if (key in obj) {
-      Object.defineProperty(obj, key, {
-        value: value,
-        enumerable: true,
-        configurable: true,
-        writable: true
-      });
-    } else {
-      obj[key] = value;
-    }
-    return obj;
+  function _defineProperty(e, r, t) {
+    return (r = toPropertyKey(r)) in e ? Object.defineProperty(e, r, {
+      value: t,
+      enumerable: !0,
+      configurable: !0,
+      writable: !0
+    }) : e[r] = t, e;
   }
 
   const formatBytes = (bytes, decimals) => {
