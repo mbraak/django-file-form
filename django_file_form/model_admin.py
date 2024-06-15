8cf8a8f16c6e55dd390e4340d859eb47f9f2efe0
Marco Braak <marco@floryn.com>
2024-06-15 12:31:45 +0200

    Add js coverage to admin

--- django_file_form/model_admin.py
+++ django_file_form/model_admin.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.forms import ModelForm
+from django.forms import ModelForm, Media
 from django.contrib import admin
 
 from .fields import UploadedFileField
@@ -22,9 +22,17 @@ class FileFormAdminMixin(with_typehint(admin.ModelAdmin)):
         }
     }
 
-    class Media:
-        css = {"all": ("file_form/file_form.css",)}
-        js = ("file_form/file_form.js", "file_form/auto_init.js")
+    @property
+    def media(self):
+        file_form_media = Media(
+            css={"all": ("file_form/file_form.css",)},
+            js=(self.get_file_form_js(), "file_form/auto_init.js")
+        )
+
+        return super().media + file_form_media
+
+    def get_file_form_js(self):
+        return "file_form/file_form.min.js"
 
     def save_model(self, request, obj, form, change):
         super().save_model(request, obj, form, change)
