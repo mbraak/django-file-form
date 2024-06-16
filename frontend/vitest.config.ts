2451e45415faa5b3b04719770ba01e0c83be0477
Marco Braak <marco@floryn.com>
2024-06-16 10:20:09 +0200

    Revert "V8 coverage"
    This reverts commit 722ca724aa9816d3d64d04937ff1854583726b9b.

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     coverage: {
-      provider: "v8",
+      provider: "istanbul",
       reporter: ["text", "json"]
     }
   }
