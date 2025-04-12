bc57013b223b4dd7bbda15dfe0b1407833962f54
Marco Braak <marco@floryn.com>
2025-04-12 21:43:38 +0200

    Test init form set (#1042)
    * Test formset
    
    * Build
    
    * Add tests
    
    * Set text using createTextNode
    
    * Build js
    
    * Correctly check if test failed
    
    * Upload artifacts
    
    * Try headless=new
    
    * Remove artifacts
    
    * Restore did_test_have_errors code
    
    * Restore headless mode; remove screenshots and logging
    
    * Rft test_form_error
    
    * Remove logging
    
    * Remove did_test_have_errors
    
    * Print javascript coverage from Selenium test
    
    * Update codecov
    
    * Output lcov for jest test
    
    * Undo print_coverage

---
+++ frontend/src/init_form_set.test.ts
@@ -0,0 +1,80 @@
+import { screen } from "@testing-library/dom";
+
+import initFormSet from "./init_form_set.ts";
+
+const createDiv = (className?: string) => {
+  const div = document.createElement("div");
+
+  if (className) {
+    div.className = className;
+  }
+
+  return div;
+};
+
+const createInput = (type: string, name: string, value?: string) => {
+  const input = document.createElement("input");
+
+  input.setAttribute("type", type);
+  input.setAttribute("name", name);
+
+  if (value) {
+    input.setAttribute("value", value);
+  }
+
+  return input;
+};
+
+const createHiddenInput = (name: string, value: string) =>
+  createInput("hidden", name, value);
+
+const createFormSet = (prefix: string) => {
+  const form = document.createElement("form");
+  document.body.append(form);
+
+  form.append(createHiddenInput(`${prefix}-TOTAL_FORMS`, "1"));
+  form.append(createHiddenInput("csrfmiddlewaretoken", "token1"));
+
+  form.append(createHiddenInput(`${prefix}-0-form_id`, "id1"));
+  form.append(createHiddenInput(`${prefix}-0-upload_url`, "/upload"));
+
+  const uploaderDiv = createDiv("dff-uploader");
+  form.append(uploaderDiv);
+
+  const containerDiv = createDiv("dff-container");
+  uploaderDiv.append(containerDiv);
+
+  containerDiv.append(createInput("file", `${prefix}-0-input_file`));
+
+  return form;
+};
+
+beforeEach(() => {
+  document.body.innerHTML = "";
+});
+
+describe(".initFormSet", () => {
+  it("initializes a form", () => {
+    const form = createFormSet("form");
+
+    initFormSet(form, {});
+
+    screen.getByText("Drop your files here");
+  });
+
+  it("initializes a form with a string prefix parameter", () => {
+    const form = createFormSet("test");
+
+    initFormSet(form, "test");
+
+    screen.getByText("Drop your files here");
+  });
+
+  it("initializes a form with a prefix in an object parameter", () => {
+    const form = createFormSet("test");
+
+    initFormSet(form, { prefix: "test" });
+
+    screen.getByText("Drop your files here");
+  });
+});
