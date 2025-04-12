35e3c47b9b6b0a5c42f29fc6c15158939895e083
Marco Braak <marco@floryn.com>
2025-04-12 21:51:03 +0200

    Fix test logging and screenshots

--- testproject/django_file_form_example/tests/utils/base_live_testcase.py
+++ testproject/django_file_form_example/tests/utils/base_live_testcase.py
@@ -16,6 +16,7 @@ class SeleniumTestMetaClass(SeleniumTestCaseBase):
             options.add_argument("--headless")
 
         options.add_argument("--disable-dev-shm-usage")
+        options.set_capability("goog:loggingPrefs", {"browser": "ALL"})
 
         return options
 
@@ -33,6 +34,8 @@ class BaseLiveTestCase(SeleniumTestCase, LiveServerTestCase, metaclass=SeleniumT
         )
         self.selenium.set_network_conditions(latency=0, throughput=-1)
 
+        self.setup_detect_test_error()
+
     def handle_coverage(self):
         if getattr(settings, "DJANGO_FILE_FORM_COVERAGE_JS", False):
             self.save_coverage()
@@ -46,6 +49,33 @@ class BaseLiveTestCase(SeleniumTestCase, LiveServerTestCase, metaclass=SeleniumT
     def tearDown(self):
         try:
             self.handle_coverage()
+            self.handleErrors()
             self.page.cleanup()
         finally:
             super().tearDown()
+
+    def setup_detect_test_error(self):
+        self.error_count = len(self._outcome.result.errors)
+        self.failure_count = len(self._outcome.result.failures)
+
+    def did_test_have_errors(self):
+        return self.error_count != len(self._outcome.result.errors) or self.failure_count != len(self._outcome.result.failures)
+
+    def handleErrors(self):
+        if self.did_test_have_errors():  # pragma: no cover
+            self.save_screenshot(self.id())
+            self.print_browser_log()
+
+    @classmethod
+    def save_screenshot(cls, method_name):  # pragma: no cover
+        screenshots_path = Path("screenshots")
+        if not screenshots_path.exists():
+            screenshots_path.mkdir()
+
+        filename = str(screenshots_path.joinpath(method_name + ".png"))
+        cls.selenium.get_screenshot_as_file(filename)
+
+    @classmethod
+    def print_browser_log(cls):  # pragma: no cover
+        for entry in cls.selenium.get_log("browser"):
+            print(entry)
