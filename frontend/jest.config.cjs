f784362fb0c2c51893935a6649e1aafcc93b6548
Marco Braak <marco@floryn.com>
2025-04-12 15:58:16 +0200

    Output lcov for jest test

--- frontend/jest.config.cjs
+++ frontend/jest.config.cjs
@@ -1,4 +1,5 @@
 module.exports = {
+  coverageReporters: ["lcov", "text"],
   setupFiles: ["./jestSetup/setupDragEvents.ts"],
   testEnvironment: "jsdom",
   transformIgnorePatterns: ["node_modules/(?!(mime|url-join)/)"]
